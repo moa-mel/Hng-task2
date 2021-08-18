66a698498f0a35df14f8fdbcd4ccff645c2e5b0a
moa-mel <52823454+moa-mel@users.noreply.github.com>
2021-08-18 10:05:52 +0100

    first commit

---
+++ Name/script.js
@@ -0,0 +1,5 @@
+let firstName = "Olaitan";
+let lastName = "Akano";
+
+console.log("My name is " + firstName + " " + lastName)
+alert("Hi, welcome to my page. My name is " + firstName + " " + lastName)
